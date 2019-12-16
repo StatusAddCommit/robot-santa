2970ed45e02884d422df5da74f5ce9d41e4f5baa
CyanBlueCode <CHASE@Chases-MacBook-Pro.local>
2019-12-15 16:44:34 -0800

    (progress) setup express, env & eslint

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const { PORT = 3000 } = process.env;
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('hello world'));
+
+app.listen(PORT, err => console.warn(err || `Listening on ${PORT}`));
