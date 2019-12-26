99f4d5121c4ac50f2ea55183bb7479bb8c8b6c98
CyanBlueCode <CHASE@Chases-MacBook-Pro.local>
2019-12-26 13:53:20 -0800

    (progress) docker config setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => res.send('hello world'));
